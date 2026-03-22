cc0c3e9dea4fd907f9fc1ef4946e3fba1575f2a1
omerhagag2511 <omerhagag2511@gmail.com>
2026-03-22 14:37:05 +0200

    1st commit

--- app/src/main/java/com/example/project12/MainActivity.java
+++ app/src/main/java/com/example/project12/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.project12;
 
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +11,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    ToggleButton tb;
+    Switch swDN
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tb = (ToggleButton) findViewById(R.id.tb);
+        swDN = (Switch) findViewById(R.id.swDN);
+
     }
 }
