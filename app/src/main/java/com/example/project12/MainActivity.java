db219964b42d9fe350488d55ef8b3adef68c59ac
omerhagag2511 <omerhagag2511@gmail.com>
2026-03-22 15:04:00 +0200

    2nd commit

--- app/src/main/java/com/example/project12/MainActivity.java
+++ app/src/main/java/com/example/project12/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.project12;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.ToggleButton;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     ToggleButton tb;
-    Switch swDN
+    ImageView iv;
+    Switch swDN;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tb = (ToggleButton) findViewById(R.id.tb);
+        tb = (ToggleButton) findViewById(R.id.Tb);
         swDN = (Switch) findViewById(R.id.swDN);
+        iv = (ImageView) findViewById(R.id.iv);
+    }
+    public void goToggle(View v) {
+        if (tb.isChecked()) {
 
+        } else {
+
+        }
     }
 }
